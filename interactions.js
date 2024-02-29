5bdf4f24688ce7d237b2b7f402c4586020b8b96c
Fevelin <evelin.flamis@student.norwichuni.ac.uk>
2024-02-29 15:03:30 +0000

    202

---
+++ interactions.js
@@ -0,0 +1,19 @@
+const colorButton = document.getElementById("color-changing-buttons");
+
+colorButton.onclick = () => {
+changeColor();
+}
+
+/**
+ * function triggered every time user scrolls
+ * @param {WheelEvent} event event containing the wheel scrolling parametres
+ */
+function mouseWheel(event) {
+    changeColor();
+    //delta means increment 
+    //- if the user is scrolling quickly it will be higher
+    const deltaWheel = event.delta * 0.5;
+
+    offset = offset + deltaWheel;
+
+}
---
+++ sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    angleMode(RADIANS); // Use radians for angles
+   }
+   
+   function draw() {
+    background(0);
+    translate(width / 2, height / 2); // Center the origin
+    stroke(255, 100, 100);
+    noFill();
+    //changeColor();
+ 
+
+    //scale(100);
+
+   
+    // Define the cannabis curve equation
+    let curveEquation = (theta) => {
+        const a = 1;
+       return a * (1 + 9/10 * cos(8 * theta)) * (1 + 0.1 * cos(24 * theta)) * (0.9 + 0.1 * cos(200 * theta)) * (1 + sin(theta));
+    };
+   
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += 0.001) {
+       let r = 100 * curveEquation(a);
+       let x = r * cos(a);
+       let y = -r * sin(a);
+       vertex(x, y);
+    }
+    endShape();
+   }
+   
