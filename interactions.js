17870fe12d546d7a04120a42c5ef25285d09b1b5
Fevelin <evelin.flamis@student.norwichuni.ac.uk>
2024-03-04 12:07:49 +0000

    02121

--- interactions.js
+++ interactions.js
@@ -8,20 +8,20 @@ changeColor();
  * function triggered every time user scrolls
  * @param {WheelEvent} event event containing the wheel scrolling parametres
  */
-////function mouseWheel(event) {
-    changeColor();
-    var angle = map(mouseX,0,width, -90, 90);
-    var r = 100;
-    //delta means increment 
-    //- if the user is scrolling quickly it will be higher
-    const deltaWheel = event.delta * 0.5;
-
-    offset = offset + deltaWheel;
-
-//}
 function mouseWheel(event) {
     const deltaWheel = event.delta * 0.5;
     setSize(-event.deltaY / 100)
+    
+
+    changeColor();
+    var angle = map(mouseX,0,width, -90, 90);
+    var r = 100;
+
+    //delta means increment 
+    //- if the user is scrolling quickly it will be higher
+
+    current_size = current_size + deltaWheel * 0.06;
+    
 }
 
 
